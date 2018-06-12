8a1efd8dd2f475c2671b0a7bba32dba611b66857
Marcio <mpschiehl@outlook.com>
2018-06-12 10:55:56 -0300

    Inicializando repositorio

---
+++ Filme.java
@@ -0,0 +1,90 @@
+
+import java.time.LocalDate;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alunos
+ */
+public class Filme {
+private String titulo, genero,tituloOriginal;
+public void setTitulo(String titulo){
+    this.titulo = titulo;
+}
+public String getTitulo(){
+    return titulo;
+}
+public void setGenero(String genero){
+    this.genero = genero;
+}
+public String getGenero(){
+    return genero;
+}
+public void setTituloOriginal(String tituloOriginal){
+    this.tituloOriginal = tituloOriginal;
+}
+public String getTituloOriginal(){
+    return tituloOriginal;
+}
+private short anoLancamento;
+public void setAnoLancamento(short anoLancamento){
+    this.anoLancamento = anoLancamento;
+}
+public short getAnolancamento(){
+    return anoLancamento;
+}
+private double valorFaturamento,valorOrcamento;
+public double getValorFaturamento() {
+        return valorFaturamento;
+    }
+public void setValorFaturamento(double valorFaturamento) {
+        this.valorFaturamento = valorFaturamento;
+    }
+public double getValorOrcamento() {
+        return valorOrcamento;
+    }
+public void setValorOrcamento(double valorOrcamento) {
+        this.valorOrcamento = valorOrcamento;
+    }
+private long espectadores;
+public long getEspectadores(){
+    return  espectadores;
+}
+public void setEspetadores(long espectadores){
+    this.espectadores=espectadores;
+}
+private String classificacao;
+public String getClassificao(){
+    return classificacao;
+}
+public void setClassificao(){
+    this.classificacao=classificacao;
+}
+private double mediaPreco;
+public double getMediaPreco(){
+    return mediaPreco;
+}
+public void setMediaPreco(){
+    this.mediaPreco = mediaPreco;
+}
+private LocalDate dataLancamentoBrasil;
+public void setDataLancamentoBrasil(){
+    this.dataLancamentoBrasil = dataLancamentoBrasil;
+}
+public LocalDate getDataLancamentoBrasil(){
+    return dataLancamentoBrasil;
+}
+private String idioma;
+public void setIdioma(){
+    this.idioma=idioma;
+}
+public String getIdioma(){
+    return idioma;
+}
+
+}
---
+++ Personagem.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alunos
+ */
+public class Personagem {
+    private String nome,cla,aldeia;
+    private long nivelChaKra;
+    private char sexo;
+    private byte idade, idadeMental;
+
+    public void setNome(String nome){
+       /* if(nome.trim().equals("")){
+            throw new
+            IllegalArgumentException("Nome não pode ser vazio!!");
+        }
+        if(nome.trim().length()<3){
+            throw new IllegalArgumentException
+        ("Nome tem que conter no minimo 3 caracteres");
+        }*/
+        this.nome = nome;
+    }
+
+    public void setCla(String cla) {
+        this.cla = cla;
+    }
+
+    public void setAldeia(String aldeia) {
+        this.aldeia = aldeia;
+    }
+
+    public void setNivelChaKra(long nivelChaKra) {
+        this.nivelChaKra = nivelChaKra;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCla() {
+        return cla;
+    }
+
+    public String getAldeia() {
+        return aldeia;
+    }
+
+    public long getNivelChaKra() {
+        return nivelChaKra;
+    }
+    
+   public void setSexo(char sexo){
+       this.sexo= sexo;
+   } 
+
+    public char getSexo() {
+        return sexo;
+    }
+    public void setIdade(){
+        this.idade = idade;
+    }
+
+    public byte getIdade() {
+        return idade;
+    }
+    public void setIdademental(byte idadeMental){
+        this.idadeMental = idadeMental;
+    }
+
+    public byte getIdadeMental() {
+        return idadeMental;
+    }
+
+    public void setIdade(byte idade) {
+        this.idade = idade;
+    }
+
+    public void setIdadeMental(byte idadeMental) {
+        this.idadeMental = idadeMental;
+    }
+  
+}
